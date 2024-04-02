9a151cf367a48e93968832a00a59a273518dce79
nav-yuht <116895242+nav-yuht@users.noreply.github.com>
2024-04-02 12:24:07 +0200

    reseni.

--- src/main/java/cz/czechitas/ukol07/Aplikace.java
+++ src/main/java/cz/czechitas/ukol07/Aplikace.java
@@ -1,7 +1,21 @@
 package cz.czechitas.ukol07;
+import java.util.List;
+import java.util.function.ToDoubleBiFunction;
 
 /**
  * Spouštěcí třída aplikace
  */
 public class Aplikace {
+    public static void main(String[] args) {
+        KnihaSluzba knihaSluzba = new KnihaSluzba();
+
+        // Celkový počet knih
+
+        // Knihy od Karla Čapka
+
+        // Knihy vydané v roce 1845
+
+
+
+    }
 }
