798bd378940c9f3ae37ff5315736a10cf9c3ec6d
nav-yuht <116895242+nav-yuht@users.noreply.github.com>
2024-04-02 12:31:08 +0200

    knihasluzba.

---
+++ src/main/java/cz/czechitas/ukol07/KnihaSluzba.java
@@ -0,0 +1,16 @@
+package cz.czechitas.ukol07;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KnihaSluzba {
+    private List <Kniha> knihy;
+
+//    public KnihaSluzba knihy(){
+//        knihy = nactiKnihyZJson();
+//    }
+}
